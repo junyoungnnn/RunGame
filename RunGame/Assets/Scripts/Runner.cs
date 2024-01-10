0f8b23118d7eb7a49d4bb9dc1a6f4f2ea0c6f9da
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-01-10 19:44:36 +0900

    (2024.01.10) 프로젝트 생성 및 리소스 추가

---
+++ RunGame/Assets/Scripts/Runner.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoadLine
+{
+    LEFT = -1,
+    MIDDLE = 0,
+    RIGHT = 1
+}
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] RoadLine roadLine;
+
+    void Start()
+    {
+        roadLine = RoadLine.MIDDLE;
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        // 왼쪽 방향키를 누르면
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (roadLine == RoadLine.MIDDLE)
+            {
+                roadLine = RoadLine.LEFT;
+            }
+            else if (roadLine == RoadLine.RIGHT)
+            {
+                roadLine = RoadLine.MIDDLE;
+            }
+        }
+
+        // 오른쪽 방향키를 누르면
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (roadLine == RoadLine.MIDDLE)
+            {
+                roadLine = RoadLine.RIGHT;
+            }
+            else if (roadLine == RoadLine.LEFT)
+            {
+                roadLine = RoadLine.MIDDLE;
+            }
+        }
+    }
+}
+
+
+//// 왼쪽 방향키를 누르면
+//if (Input.GetKeyDown(KeyCode.LeftArrow))
+//{
+//    if (roadLine <= RoadLine.LEFT)
+//    {
+//        roadLine = RoadLine.LEFT;
+//    }
+//    else
+//    {
+//        roadLine++;
+//    }
+//}
+
+//오른쪽 방향키를 누르면
+//        if (Input.GetKeyDown(KeyCode.RightArrow))
+//        {
+//    if (roadLine >= RoadLine.RIGHT
+//            {
+//        roadLine = RoadLine.RIGHT;
+//    }
+//    else
+//    {
+//        roadLine--;
+//    }
+//}
